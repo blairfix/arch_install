f7562905c8c569ae5efc08676f9fc7bfca1fe0a9
Blair Fix <blairfix@gmail.com>
2022-02-10 16:17:05 -0500

    changed git to https

--- scripts/stage2/05_dots.sh
+++ scripts/stage2/05_dots.sh
@@ -3,7 +3,7 @@
 echo install dot files
 
 # download dot file repository from github
-git clone git@github.com:blairfix/blair_dots.git
+git clone https://github.com/blairfix/blair_dots.git
 
 # run install script 
 cd ./blair_dots

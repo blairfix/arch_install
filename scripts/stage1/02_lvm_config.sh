693347d2ab302d9daa28e1c3c5a51672d07e96c2
Blair Fix <blairfix@gmail.com>
2022-02-10 11:14:43 -0500

    fixed typo in lvm config

--- scripts/stage1/02_lvm_config.sh
+++ scripts/stage1/02_lvm_config.sh
@@ -3,6 +3,7 @@
 
 # config lvm
 #---------------------------------------------
+echo configuring lvm
 
 # add lvm2 to mkinitcpio.conf
 # got to hooks, add 'lvm2' between block and filesystems
@@ -12,14 +13,14 @@ cp /etc/mkinitcpio.conf /etc/mkinitcpio.conf.bak
 
 
 # search and replace in config file
-ffind_string='modconf block filesystems'
+find_string='modconf block filesystems'
 replace_string='modconf block lvm2 filesystems'
 sed -i "s|$find_string|$replace_string|g" /etc/mkinitcpio.conf
 
 
 # install config
-#mkinitcpio -p linux
-#mkinitcpio -p linux-lts
+mkinitcpio -p linux
+mkinitcpio -p linux-lts
 
 
 

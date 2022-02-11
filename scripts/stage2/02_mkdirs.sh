09a0fb56a07547a88e06963401f1b8b33ea3f949
Blair Fix <blairfix@gmail.com>
2022-02-11 09:31:19 -0500

    changed r_package script

--- scripts/stage2/02_mkdirs.sh
+++ scripts/stage2/02_mkdirs.sh
@@ -4,7 +4,7 @@
 echo making home directories
 
 cd /home/blair/ 
-mkdir cronjobs Desktop Documents Downloads Dropbox Multimedia ipvanish Projects Research
+mkdir cronjobs Desktop Documents Downloads Dropbox Multimedia ipvanish Projects Research R
 
 # directory for backup drive
 mkdir -p /media/blair/Backup_drive

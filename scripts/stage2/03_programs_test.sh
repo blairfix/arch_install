c3124ff2597bf44072f43ca756f41900741ed4ae
Blair Fix <blairfix@gmail.com>
2022-02-11 05:29:10 -0500

    added git ignore and pacman test

---
+++ scripts/stage2/03_programs_test.sh
@@ -0,0 +1,28 @@
+
+# install programs
+echo installing core programs
+
+
+# microcode
+#--------------------------------------------------
+echo "Which version of microcode do you want? (amd, intel)"
+read micro
+
+if [ $micro == "amd" ] 
+then
+    # amd microcode
+    pacman -S amd-ucode	    
+else
+    # intel microcode
+    pacman -S intel-ucode	   
+fi
+
+# install software list
+#--------------------------------------------------
+
+# make pacman list
+cd software_lists
+./get_packages.sh pacman
+
+# pipe list to pacman
+cat sf_list | pacman -S

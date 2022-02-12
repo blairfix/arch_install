83dc27f4bcf193ea03217fd5760aa76670e4938c
Blair Fix <blairfix@gmail.com>
2022-02-12 07:39:37 -0500

    don't make dropbox folder

--- stage2/01_mkdirs.sh
+++ stage2/01_mkdirs.sh
@@ -4,6 +4,6 @@
 echo making home directories
 
 cd /home/blair/ 
-mkdir cronjobs Backup_drive Desktop Documents Downloads Dropbox Multimedia ipvanish Projects Research R
+mkdir cronjobs Backup_drive Desktop Documents Downloads Multimedia ipvanish Projects Research R
 
 

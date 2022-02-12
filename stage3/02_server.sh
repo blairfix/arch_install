583776dab220ee1470aa6632199dcd7a9363dfd4
blair <blair@blairfix@gmail.com>
2022-02-12 10:03:02 -0500

    better instructions in server setup

--- stage3/02_server.sh
+++ stage3/02_server.sh
@@ -6,13 +6,19 @@ sudo systemctl start jellyfin
 sudo systemctl enable jellyfin 
 
 # setup rclone
+echo '----------------------------------------------------'
 echo Setting up rclone ...
-echo 'Please type: rclone config'
-echo Select new remote
-echo The name of the remote is remote
+echo ' '
+echo '1. Please type: rclone config'
+echo '2. Select new remote'
+echo '3. The name of the remote is "remote"'
+echo '4. Drobox should be option 11'
+
 alacritty 
 
 # setup firefox profile for selenium
+echo '----------------------------------------------------'
+echo ' '
 echo Time to make a firefox profile for selenium
 echo "1. Make a profile called 'scrape'"
 echo "2. Download the subscriber csv and tell firefox to save it in downloads dir" 

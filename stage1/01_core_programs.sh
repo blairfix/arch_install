6d98ce019aaa1da58da56d7792126bf64a57d3dd
Blair Fix <blairfix@gmail.com>
2022-02-13 09:26:38 -0500

    fixed stage 1 software install

--- stage1/01_core_programs.sh
+++ stage1/01_core_programs.sh
@@ -2,8 +2,12 @@
 
 echo installing core packages
 
-# pipe software list to pacman
-pacman -S $(< core_soft)
+# make pacman list
+cd software_lists
+./get_packages.sh core_soft
+
+# pipe list to pacman
+pacman -S $(< sf_list)
 
 
 # enable network manager

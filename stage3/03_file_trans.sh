4ec9f0c22b0ea50bfa7676763160ace8214c7c99
blair <blair@blairfix@gmail.com>
2022-02-17 14:54:51 -0500

    renumbered stage 3 scripts

--- stage3/03_file_trans.sh
+++ stage3/03_file_trans.sh
@@ -16,17 +16,24 @@ read bak_path
 # mount backup drive
 sudo mount $bak_path /media/blair/Backup_drive
 
+
+
+# rsync
+#-------------------------------------------------------
+
+# copy cronjobs
+rsync -avzh /media/blair/Backup_drive/cronjobs/ /home/blair/cronjobs
+
 # copy Desktop
 rsync -avzh /media/blair/Backup_drive/Desktop/ /home/blair/Desktop
+
+# copy Documents
+rsync -avzh /media/blair/Backup_drive/Documents/ /home/blair/Documents
 
 # copy Projects
 rsync -avzh /media/blair/Backup_drive/Projects/ /home/blair/Projects
 
 # copy Research
 rsync -avzh /media/blair/Backup_drive/Research/ /home/blair/Research
-
-# copy Documents
-rsync -avzh /media/blair/Backup_drive/Documents/ /home/blair/Documents
-
 
 

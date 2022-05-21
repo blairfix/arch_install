fc484404e41f4674215e062ebb22de8754b36044
blair <blair@blairfix@gmail.com>
2022-05-20 20:53:24 -0400

    moved timezone to stage 2

---
+++ stage2/09_timezone.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo setting time zone to Toronto 
+
+# timedatectl list-timezones
+timedatectl set-timezone America/Toronto
+
+
+# enable time synchronization with systemmd
+systemctl enable systemd-timesyncd
+
+

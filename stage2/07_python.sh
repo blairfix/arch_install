72f9564ae9bfe3db5f7eb302f02e4e78dbe9b4e7
blairfix <blairfix@gmail.com>
2022-06-09 19:36:40 -0400

    removed blueman

--- stage2/07_python.sh
+++ stage2/07_python.sh
@@ -1,7 +1,7 @@
 # python libraries
 #--------------------------------------------------------------------
 
-# python 2
+# python 
 pip install selenium
 pip install pandas
 pip install tqdm
@@ -10,16 +10,5 @@ pip install grip
 pip install imutils
 pip install scikit-build
 pip install prompt_toolkit
-
-
-# python 3
-pip3 install selenium
-pip3 install pandas
-pip3 install tqdm
-pip3 install datetime
-pip3 install grip
-pip3 install imutils
-pip3 install scikit-build
-pip3 install prompt_toolkit
-
-
+pip install pynput
+pip install pyautogui

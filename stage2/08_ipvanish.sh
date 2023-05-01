0c428f3c3d3beae520d95d9cf92ea9a504a00f31
blairfix <blairfix@gmail.com>
2023-05-01 08:53:45 -0400

    update pacman and ipvanish

--- stage2/08_ipvanish.sh
+++ stage2/08_ipvanish.sh
@@ -2,6 +2,16 @@
 
 cd ~/ipvanish
 
+# clean old files
+rm *.ovpn
+
+# download config files
 wget https://www.ipvanish.com/software/configs/configs.zip
 unzip configs.zip
+
+# fix keysize error
+for f in *.ovpn
+do
+    sed -i '/keysize/d' "$f" 
+done
 

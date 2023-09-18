3b5cf9be5c011ab2bc9d1a8a6eded354c04d212c
blairfix <blairfix@gmail.com>
2023-09-18 16:40:16 -0600

    added tidy quant

--- stage2/07_python.sh
+++ stage2/07_python.sh
@@ -2,30 +2,30 @@
 #--------------------------------------------------------------------
 
 # python 
-pip install selenium
-pip install pandas
-pip install tqdm
-pip install datetime
-pip install grip
-pip install imutils
-pip install scikit-build
-pip install prompt_toolkit
-pip install pynput
-pip install pyautogui
-pip install proselint
+pip install selenium -U --break-system-packages
+pip install pandas -U --break-system-packages
+pip install tqdm -U --break-system-packages
+pip install datetime -U --break-system-packages
+pip install grip -U --break-system-packages
+pip install imutils -U --break-system-packages
+pip install scikit-build -U --break-system-packages
+pip install prompt_toolkit -U --break-system-packages
+pip install pynput -U --break-system-packages
+pip install pyautogui -U --break-system-packages
+pip install proselint -U --break-system-packages
 
 # radian
-pip install -U radian
+pip install radian -U --break-system-packages
 
 # linode
-pip install linode-cli
-pip install boto
+pip install linode-cli -U --break-system-packages
+pip install boto -U --break-system-packages
 
 # ssl
-pip install cryptography requests
+pip install cryptography requests -U --break-system-packages
 
 # pip auto update
-pip-review
+#pip-review -U --break-system-packages
 
 # linode in virtual environment
 python -m venv .env
